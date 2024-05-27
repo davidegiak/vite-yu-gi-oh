56b8049ce2197efb1976845260e727a84e8ab13a
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-05-27 20:08:18 +0200

    bonus 1

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
-<script setup>
+<script>
 import MainApp from './components/MainApp.vue'
+import cards from './data/cards.js'
+export default {
+    components:{
+        MainApp
+    },
+    data(){
+        return {
+            cards
+        }
+    },
+    mounted(){
+        axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0").then(function (response) {
+        // handle success
+        // cards.personaggi = response .data.data.results;
+        // console.log(response.data.data)
+        cards.dati = response.data.data
+        console.log(cards.dati);
+  })
+    }
+}
+
 </script>
 
 <template>
--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -8,7 +8,13 @@ export default {
     data() {
         return{
         }
-    }
+    },
+//     created() {
+//     axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0").then(risultato => {
+//       this.cards.data = risultato.data.results;
+//     });
+//   },
+
 }
 </script>
 
